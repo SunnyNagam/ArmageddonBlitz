d433555fe68ccc8f5fc5061a605fd14e559aee06
Jack Glass <jackadrianglass@gmail.com>
2020-01-22 20:47:44 -0700

    Implemented the init function to include pygame stuff
    Still doesn't run

--- gameState.py
+++ gameState.py
@@ -1,33 +1,50 @@
+import pygame
+import block
+import bot
+import jack
+import sunny
+
 class GameState:
 
 	def __init__(self, bWidth = 200, bHeight = 100):
-		Bots = list()
-		Board = new Block[bWidth][bHeight]
+		pygame.init()
 
-	def AddBot( bot : Bot ):
-		Bots.push_back( bot )
+		self.Bots = list()
+		self.Board = list()
+		self.clock = pygame.time.Clock()
+		self.gameDisplay = pygame.display.set_mode((800,600))
+		pygame.display.set_caption("Armageddon Blitz")
+
+		for row in range(bWidth):
+			self.Board[row].append( list() )
+			for col in range(bHeight):
+				self.Board[row][col] = Block()
+
+	def AddBot(self, bot : Bot ):
+		self.Bots.append( bot )
 
 	def RunGameLoop( self ):
-		while( true ):
+		while( True ):
 			self.Update()
 			self.Draw()
+			self.clock.tick(60)
 
 	def Update(self):
-		for( bot in Bots ):
+		for bot in self.Bots:
 			bot.Update()
 
 	def Draw(self):
-		for y in range(0, len(Board)):
-			for x in range(0, len(Board[y])):
+		for y in range(0, len(self.Board)):
+			for x in range(0, len(self.Board[y])):
 
-				block = Board[y][x]
+				block = self.Board[y][x]
 
-				for bot in Bots: 
+				for bot in self.Bots: 
 					if block.owner == bot.id:
 						drawRect(x*10, y*10, (x+1)*10, (y+1)*10, bot.color)
 
 #MAIN
-game = new GameState()
+game = GameState()
 game.AddBot(Sunny())
 game.AddBot(Jack())
 game.RunGameLoop()
--- jack.py
+++ jack.py
@@ -1,6 +1,8 @@
 from bot import *
 
 class Jack(Bot):
+	pid = 420
+	colour = "sky blue"
 	def Update(self):
 		#do something to the bot's stuff
 		pass
