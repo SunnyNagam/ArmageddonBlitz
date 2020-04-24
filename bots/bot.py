d02f7ce388a56e0191c59f40477a862a56c58b3d
Jack Glass <jackadrianglass@gmail.com>
2020-04-23 20:45:31 -0600

    Enjoy some spaces instead of tabs

--- bots/bot.py
+++ bots/bot.py
@@ -20,8 +20,8 @@ class Dir(Enum):
 
 class BotState:
     """
-	Class that the game keeps track of for the players position
-	"""
+    Class that the game keeps track of for the players position
+    """
 
     def __init__(self, pid: int, pos: (int, int), color: Color):
         self.pid = pid
@@ -31,9 +31,9 @@ class BotState:
 
     def move(self, action: Move) -> None:
         """
-		Updates the bot's position and direction based on the action given
-		:param action: Action to perform
-		"""
+        Updates the bot's position and direction based on the action given
+        :param action: Action to perform
+        """
         if action == Move.IDLE:
             return
 
@@ -71,22 +71,22 @@ class BotState:
 
 class Bot:
     """
-	The base class to inherit from for each player
-	"""
+    The base class to inherit from for each player
+    """
 
     def __init__(self, pid: int):
         self.pid = pid
 
     def move(self, board: [[int]], bot_state: BotState) -> Move:
         """
-		Function to be implemented by bot to make a decision of where to move
-		:param bot_state: Current state of the bot
-			DO NOT MODIFY
-			If you do, you're a f***ing coward and a cheat
-		:param board: 2d array representing the game board
-		value 0 means that the block has not been claimed
-		any other value represents the id of the player who owns it
-		:return: The move that the bot wants to take
-		"""
+        Function to be implemented by bot to make a decision of where to move
+        :param bot_state: Current state of the bot
+            DO NOT MODIFY
+            If you do, you're a f***ing coward and a cheat
+        :param board: 2d array representing the game board
+        value 0 means that the block has not been claimed
+        any other value represents the id of the player who owns it
+        :return: The move that the bot wants to take
+        """
         print(colored(f"Not implemented for bot {self.pid}", "red"))
         return Move.IDLE
